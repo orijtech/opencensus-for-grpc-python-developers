ef02dd2ba59afc0ffd6d6eebbd5c72069ca902a8
Emmanuel T Odeke <emmanuel@orijtech.com>
2018-05-24 00:01:38 -0700

    server: add OpenCensus instrumentation for the server

--- README.md
+++ README.md
@@ -3,14 +3,23 @@ OpenCensus for gRPC Python developers
 
 ## Building it
 
-Install Python3 then use pip3
+Install `Python3` then use `pip3`
 or
-Python3 then use pip
+`Python2` then use `pip`
 
 ### Dependencies
+
+With Python2
 ```shell
-pip3 install grpcio-tools
+pip3 install grpcio-tools opencensus
 ```
+
+OR
+
+With Python2
+```shell
+pip install grpcio-tools opencensus
+````
 
 ### Generate the protobuf definitions
 ```shell
--- capitalizeServer.py
+++ capitalizeServer.py
@@ -15,21 +15,45 @@
 # limitations under the License.
 
 import grpc
+import os
 import time
 from concurrent import futures
 
 import defs_pb2_grpc as proto
 import defs_pb2 as pb
 
+# Tracing related imports
+from opencensus.trace.exporters import stackdriver_exporter
+from opencensus.trace.exporters.transports.background_thread import BackgroundThreadTransport
+from opencensus.trace.samplers import always_on
+from opencensus.trace.tracer import Tracer
+from opencensus.trace.ext.grpc import server_interceptor
+
+# Create the exporters:
+# 1. Stackdriver
+stackdriverExporter = stackdriver_exporter.StackdriverExporter(
+            project_id=os.environ.get('OCGRPC_PROJECTID', 'census-demos'),
+            transport=BackgroundThreadTransport)
+
 class CapitalizeServer(proto.FetchServicer):
     def __init__(self, *args, **kwargs):
         super(CapitalizeServer, self).__init__()
 
     def Capitalize(self, request, context):
-        return pb.Payload(data=request.data.upper())
+        tracer = Tracer(sampler=always_on.AlwaysOnSampler(), exporter=stackdriverExporter)
+        with tracer.span(name='Capitalize') as span:
+            data = request.data
+            span.add_annotation('Data in', len=len(data))
+            return pb.Payload(data=data.upper())
 
 def main():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    # Setup and start the gRPC server with the OpenCensus integration/interceptor
+    tracer_interceptor = server_interceptor.OpenCensusServerInterceptor(
+            always_on.AlwaysOnSampler(), stackdriverExporter)
+
+    server = grpc.server(
+            futures.ThreadPoolExecutor(max_workers=10),
+            interceptors=(tracer_interceptor,))
     proto.add_FetchServicer_to_server(CapitalizeServer(), server)
     server.add_insecure_port('[::]:9778')
     server.start()
